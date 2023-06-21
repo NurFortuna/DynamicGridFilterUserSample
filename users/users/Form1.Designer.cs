37bb18bff79a1fbea9e623347525cfed660fb73a
NurFortuna <aysenurfortuna@gmail.com>
2023-06-21 20:16:24 +0300

    initial commit

---
+++ users/users/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace users
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dataGridView1 = new DataGridView();
+            label1 = new Label();
+            textBox1 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            buttonSearch = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(12, 76);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowTemplate.Height = 25;
+            dataGridView1.Size = new Size(776, 316);
+            dataGridView1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.ForeColor = SystemColors.ControlDarkDark;
+            label1.Location = new Point(12, 29);
+            label1.Name = "label1";
+            label1.Size = new Size(70, 28);
+            label1.TabIndex = 1;
+            label1.Text = "Search";
+            label1.Click += label1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(106, 31);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(533, 23);
+            textBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.ControlDarkDark;
+            button1.Location = new Point(683, 406);
+            button1.Name = "button1";
+            button1.Size = new Size(96, 30);
+            button1.TabIndex = 3;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click_1;
+            // 
+            // button2
+            // 
+            button2.BackColor = SystemColors.ControlDarkDark;
+            button2.Location = new Point(22, 406);
+            button2.Name = "button2";
+            button2.Size = new Size(91, 32);
+            button2.TabIndex = 4;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click_1;
+            // 
+            // buttonSearch
+            // 
+            buttonSearch.BackColor = SystemColors.ControlDarkDark;
+            buttonSearch.Location = new Point(655, 23);
+            buttonSearch.Name = "buttonSearch";
+            buttonSearch.Size = new Size(87, 34);
+            buttonSearch.TabIndex = 5;
+            buttonSearch.Text = "Search";
+            buttonSearch.UseVisualStyleBackColor = false;
+            buttonSearch.Click += buttonSearch_Click_1;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ControlLightLight;
+            ClientSize = new Size(800, 450);
+            Controls.Add(buttonSearch);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Controls.Add(dataGridView1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private DataGridView dataGridView1;
+        private Label label1;
+        private TextBox textBox1;
+        private Button button1;
+        private Button button2;
+        private Button buttonSearch;
+    }
+}
---
+++ users/users/Form1.cs
@@ -0,0 +1,178 @@
+﻿using Microsoft.VisualBasic.ApplicationServices;
+using Newtonsoft.Json;
+
+namespace users
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeDataGridView();
+            LoadUserList();
+            SetButtonIcons();
+
+        }
+
+        private List<User> userList;
+        private List<User> filteredList;
+        private const int PageSize = 100;
+        private int currentPage = 1;
+        private int totalPageCount;
+
+
+        class User
+        {
+            public string ID { get; set; }
+            public string JobTitle { get; set; }
+            public string EmailAddress { get; set; }
+            public string FirstNameLastName { get; set; }
+
+            public User(string id, string jobTitle, string emailAddress, string firstNameLastName)
+            {
+                ID = id;
+                JobTitle = jobTitle;
+                EmailAddress = emailAddress;
+                FirstNameLastName = firstNameLastName;
+            }
+        }
+
+        private void InitializeDataGridView()
+        {
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridView1.DefaultCellStyle.ForeColor = Color.Black;
+            dataGridView1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+
+        }
+
+        private void LoadUserList()
+        {
+            string jsonFilePath = "C:\\Users\\Lenovo\\Desktop\\user\\ExportJson.json";
+
+            try
+            {
+                string jsonContent = System.IO.File.ReadAllText(jsonFilePath);
+                userList = JsonConvert.DeserializeObject<List<User>>(jsonContent);
+                filteredList = userList.ToList();
+                dataGridView1.DataSource = userList;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Hata: " + ex.Message);
+            }
+        }
+
+        private void Search(string searchText)
+        {
+            if (!string.IsNullOrEmpty(searchText))
+            {
+                filteredList = userList.FindAll(u =>
+                    (u.ID?.Contains(searchText, StringComparison.OrdinalIgnoreCase) ?? false) ||
+                    (u.JobTitle?.Contains(searchText, StringComparison.OrdinalIgnoreCase) ?? false) ||
+                    (u.EmailAddress?.Contains(searchText, StringComparison.OrdinalIgnoreCase) ?? false) ||
+                    (u.FirstNameLastName?.Contains(searchText, StringComparison.OrdinalIgnoreCase) ?? false)
+                );
+            }
+            else
+            {
+                filteredList = userList.ToList();
+            }
+
+            //
+            totalPageCount = (int)Math.Ceiling((double)filteredList.Count / PageSize);
+            currentPage = Math.Min(currentPage, totalPageCount);
+
+            UpdateDataGridView();
+            UpdatePaginationButtons();
+        }
+
+        private void UpdateDataGridView()
+        {
+            if (filteredList.Count > 0)
+            {
+                int startIndex = (currentPage - 1) * PageSize;
+                int count = Math.Min(PageSize, filteredList.Count - startIndex);
+                List<User> pagedData = filteredList.GetRange(startIndex, count);
+                dataGridView1.DataSource = pagedData;
+            }
+            else
+            {
+                dataGridView1.DataSource = null; // Verileri temizle
+            }
+        }
+
+        private void UpdatePaginationButtons()
+        {
+            if (filteredList.Count > 0)
+            {
+                int startIndex = (currentPage - 1) * PageSize;
+                int count = Math.Min(PageSize, filteredList.Count - startIndex);
+                List<User> pagedData = filteredList.Skip(startIndex).Take(count).ToList();
+                dataGridView1.DataSource = pagedData;
+            }
+            else
+            {
+                dataGridView1.DataSource = null; // Verileri temizle
+            }
+
+
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.Text = string.Empty;
+            Search(string.Empty);
+
+        }
+
+        private void SetButtonIcons()
+        {
+            button1.Image = Image.FromFile("C:\\Users\\Lenovo\\Desktop\\user\\img\\right.png"); button1.Image = new Bitmap(button1.Image, new Size(20, 20));
+            button1.ImageAlign = ContentAlignment.MiddleCenter;
+
+            button2.Image = Image.FromFile("C:\\Users\\Lenovo\\Desktop\\user\\img\\left.png"); // Sol ok resmi i�in dosya yolunu belirtin
+            button2.Image = new Bitmap(button2.Image, new Size(20, 20));
+            button2.ImageAlign = ContentAlignment.MiddleCenter;
+
+
+            button1.Text = "";
+            button2.Text = "";
+
+            button1.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            button2.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+
+        }
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonSearch_Click_1(object sender, EventArgs e)
+        {
+            string searchText = textBox1.Text;
+            currentPage = 1;
+            Search(searchText);
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            if (currentPage < totalPageCount)
+            {
+                currentPage++;
+                Search(textBox1.Text);
+            }
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            if (currentPage > 1)
+            {
+                currentPage--;
+                Search(textBox1.Text);
+            }
+        }
+    }
+}
